1b811c97ae18d39e8d7fa79fe25d978c88801f5f
ahmedzaafran <skimaghazi@gmail.com>
2025-03-26 15:59:32 +0100

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
     // Input 1:
     ReceiptService receipt1 = new ReceiptService();
     receipt1.addProduct(new Product("book", 12.49, false, true));
-    receipt1.addProduct(new Product("music CD", 14.99, false, false));
+    receipt1.addProduct(new Product("music CD ", 14.99, false, false));
     receipt1.addProduct(new Product("chocolate bar ", 0.85, false, true));
     receipt1.printReceipt();
 
